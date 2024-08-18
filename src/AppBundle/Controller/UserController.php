d5fd7ba50a8b487dfb14619c60454fe75be2da37
tonytillet <tony.tillet@gmail.com>
2024-08-19 00:18:15 +0200

    dev env

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -29,16 +29,21 @@ class UserController extends Controller
         $form->handleRequest($request);
 
         if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $password = $this->get('security.password_encoder')->encodePassword($user, $user->getPassword());
-            $user->setPassword($password);
+            try {
+                $em = $this->getDoctrine()->getManager();
+                $password = $this->get('security.password_encoder')->encodePassword($user, $user->getPassword());
+                $user->setPassword($password);
 
-            $em->persist($user);
-            $em->flush();
+                $em->persist($user);
+                $em->flush();
 
-            $this->addFlash('success', "L'utilisateur a bien été ajouté.");
+                $this->addFlash('success', "L'utilisateur a bien été ajouté.");
 
-            return $this->redirectToRoute('user_list');
+                return $this->redirectToRoute('user_list');
+            } catch (\Exception $e) {
+                $this->addFlash('error', "L'utilisateur n'a pas pu être ajouté. Veuillez réessayer.");
+            }
+
         }
 
         return $this->render('user/create.html.twig', ['form' => $form->createView()]);
